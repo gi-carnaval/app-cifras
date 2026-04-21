b19bfd6ab529b9213fb81a55fa5c50cba711b637
Giovani Carnaval <gi.carnaval@hotmail.com>
2026-04-21 04:35:28 -0300

    update domain

---
+++ domain/repositories/auth.repository.ts
@@ -0,0 +1,36 @@
+export interface RegisterUserInput {
+  name: string
+  email: string
+  password: string
+  passwordConfirm: string
+}
+
+export interface LoginUserInput {
+  email: string
+  password: string
+}
+
+export interface RegisteredUser {
+  id: string
+  name: string
+  email: string
+}
+
+export interface AuthenticatedUser {
+  id: string
+  name: string
+  email: string
+}
+
+export interface AuthSession {
+  isAuthenticated: boolean
+  user: AuthenticatedUser | null
+}
+
+export interface AuthRepository {
+  register(input: RegisterUserInput): Promise<RegisteredUser>
+  requestEmailVerification(email: string): Promise<boolean>
+  login(input: LoginUserInput): Promise<AuthenticatedUser>
+  getCurrentSession(serializedSession?: string): Promise<AuthSession>
+  clearSession(): Promise<AuthSession>
+}
